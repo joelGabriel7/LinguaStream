3e15b780ab6a0a5bde3b653c6538c5ea57367861
Joel <joelgerman671@gmail.com>
2024-10-14 13:49:04 -0400

    set up models to save the user preferences

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 
 class Users(SQLModel, table=True):
@@ -7,3 +7,11 @@ class Users(SQLModel, table=True):
     email: str = Field(index=True, nullable=False)
     name: str = Field(index=True)
     hashed_password: str = Field(nullable=False)
+    preferences: Optional["UserPreferences"] = Relationship(back_populates="user")
+
+class UserPreferences(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='users.id')
+    source_language: str = Field(default=None)
+    target_language: str = Field(default=None)
+    user: Optional['Users'] = Relationship(back_populates='preferences')
