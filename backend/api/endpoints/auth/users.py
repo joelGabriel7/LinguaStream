04797b00ac6a66fd12163aceeeec83c694886a39
Joel <joelgerman671@gmail.com>
2024-10-14 15:30:16 -0400

    expose endpoint to set_preferences ready

--- backend/api/endpoints/auth/users.py
+++ backend/api/endpoints/auth/users.py
@@ -1,19 +1,28 @@
 from databases import get_session
 from core.security import get_current_user
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from typing import Annotated
 from models.users import Users
-from sqlmodel import select,Session
+from sqlmodel import select, Session
+from services.user_preferences_services import set_preferences_services
+from schemas.auth import UserPreferences
 
 
-user_router = APIRouter(prefix='/user', tags=['Users'])
+user_router = APIRouter(prefix="/user", tags=["Users"])
 UserDep = Annotated[Users, Depends(get_current_user)]
+SessionDep = Annotated[Session, Depends(get_session)]
 
-@user_router.get('/me')
+
+@user_router.get("/me")
 async def current_user_endpoint(
-    current_user: UserDep,
-    db: Session = Depends(get_session)
+    current_user: UserDep, db: Session = Depends(get_session)
 ):
     query = select(Users).where(Users.id == current_user.id)
-    result = db.exec(query) 
+    result = db.exec(query)
     return result.first()
+
+
+@user_router.post("/preferences", status_code=status.HTTP_201_CREATED)
+async def set_preferences(user: UserDep, preferences: UserPreferences, db: SessionDep):
+    await set_preferences_services(preferences, user, db)
+    return {"message": "Preferences languages set up successfully"}
