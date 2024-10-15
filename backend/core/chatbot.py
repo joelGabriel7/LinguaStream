3662f1b70e93ac019249f7ca61f2a9eac2c2c8e9
Joel <joelgerman671@gmail.com>
2024-10-14 21:09:47 -0400

    Logic to chatbot translate implement

---
+++ backend/core/chatbot.py
@@ -0,0 +1,30 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+from .translate import translate_text
+
+load_dotenv()
+
+
+def chatbot_ai(user_input: str, target_language: str):
+    try:
+        # Configurar la API de Generative AI
+        genai.configure(api_key=os.environ["API_KEY"])
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        # Generar contenido usando el modelo proporcionado
+        response = model.generate_content(
+            user_input,
+            generation_config=genai.types.GenerationConfig(
+                candidate_count=1,
+                stop_sequences=["x"],
+                max_output_tokens=20,
+                temperature=0.1,
+            ),
+        )
+
+        # Traducir la respuesta generada al lenguaje objetivo
+        translated_response = translate_text(target_language, response.text)
+
+        return translated_response
+    except Exception as e:
+        return f"ERROR: {str(e)}"
