90c5e5fa156ede55d8a8ef734309fae9c348d506
Joel <joelgerman671@gmail.com>
2024-10-15 20:10:01 -0400

    initial set up to using context api and global state for authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthLayout from './layouts/AuthLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout/>}> 
-            <Route index element={<Login/>}/>
-            <Route path='register' element={<Register/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+          <Routes>
+            <Route path='/' element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path='register' element={<Register />} />
+            </Route>
+          </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
